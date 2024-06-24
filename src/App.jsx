0c19ddf29c42d5a49b603f59f62df3aa1eebeaf1
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-24 16:30:24 +0800

    copy

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import AuroraBackground from "./components/AuroraBackground";
+import LayoutGrid from "./components/TilesProjects";
+import {
+  TextRevealCard,
+  // TextRevealCardDescription,
+  // TextRevealCardTitle,
+} from "./components/TextRevealCard";
+import { TracingBeam } from "./components/TracingBeam";
+import dummyContent from "./components/dummy";
+// import LayoutGrid from "./components/LayoutGrid"; // Adjust the import path as needed
+import { twMerge } from "tailwind-merge";
+import TracingBeamDemo from "./components/TBDemo";
+import GoogleGeminiEffectDemo from "./components/GoogleCompon";
+import TextGenerateEffect from "./components/TextEffect";
+import { Button } from "./components/Button";
+import MySkills from "./components/MySkills";
+import { LinkPreviewDemo } from "./components/ContactMeComp";
+import { FaArrowLeft } from "react-icons/fa";
+import ContactMe from "./components/ContactEmailMy";
+
+import {
+  SkeletonOne,
+  SkeletonTwo,
+  SkeletonThree,
+  SkeletonFour,
+} from "./components/skeletons/Skeleton";
+import { useNavigate } from "react-router-dom";
+
+const cards = [
+  {
+    id: 1,
+    content: <SkeletonOne />,
+    className: "md:col-span-2",
+    thumbnail: "1.png",
+  },
+  {
+    id: 2,
+    content: <SkeletonTwo />,
+    className: "col-span-1",
+    thumbnail: 'shop.png'
+  },
+  {
+    id: 3,
+    content: <SkeletonThree />,
+    className: "col-span-1",
+    thumbnail: 'crypto.png'
+  },
+  {
+    id: 4,
+    content: <SkeletonFour />,
+    className: "md:col-span-2",
+    thumbnail:
+      "https://images.unsplash.com/photo-1475070929565-c985b496cb9f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+const words =
+  " I'm a self-taught developer with experience in various technologies including React, Node.js, and more.";
+
+function App() {
+  const navigate = useNavigate()
+  return (
+    <div className="flex h-screen  overflow-hidden relative justify-center ">
+      {/* Left Static Side */}
+      <AuroraBackground>
+        <div className="w-1/2 text-black flex flex-col items-center justify-center p-8 min-w-[350px]">
+          <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+          <p className="text-lg mb-8">
+            Hi, I'm [Your Name], a Full Stack Developer with a passion for
+            creating amazing web applications.
+          </p>
+
+          <Button
+            borderRadius="1.75rem"
+            className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            onClick={()=> navigate('/email')}
+         >
+            Contact Me
+          </Button>
+        </div>
+        <LayoutGrid cards={cards} />
+      </AuroraBackground>
+
+      {/* Right Scrollable Side */}
+      <div className="w-1/2 bg-gray-100 overflow-y-scroll  relative min-w-[350px]">
+        <div className="min-h-screen   p-8 flex items-center justify-center ">
+          <div>
+            <h2 className="text-2xl font-bold mb-2">About Me</h2>
+
+            <TextGenerateEffect words={words} />
+          </div>
+        </div>
+
+        <div className="min-h-screen p-8 flex items-center justify-center ">
+          <FaArrowLeft size={40} />
+          <TextRevealCard text="Look!" revealText="at my projects" />
+        </div>
+
+        {/* <ContactMe/> */}
+
+        <MySkills />
+
+        <div className="container mx-auto p-8 items-center justify-center">
+          <LinkPreviewDemo />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MySkills.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+
+const MySkills = () => {
+  return (
+    <div className="container mx-auto p-8 ">
+      <h1 className="text-3xl font-bold mb-8 text-center">My Skills</h1>
+      <div className="grid lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 gap-4">
+        <div className=" transform transition-transform duration-500 hover:scale-110 hover:rotate-3">
+          <div className=" max-w-sm mx-auto bg-white border border-gray-300 rounded-lg shadow-md overflow-hidden">
+            <div className="bg-blue-500 text-white p-4 flex items-center">
+            <img  src='front.png'  alt="React Logo" className="h-6 w-6 mr-2" />
+
+              <h2 className="text-xl font-semibold">frontend</h2>
+            </div>
+            <div className="bg-gray-100 p-4">
+                <ul className="text-gray-700">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JS</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>TailwindCSS</li>
+                  <li>OAuth</li>
+                </ul>
+            </div>
+          </div>
+          
+        </div>
+        <div className=" transform transition-transform duration-500 hover:scale-110 hover:rotate-3">
+        <div className=" max-w-sm mx-auto bg-white border border-gray-300 rounded-lg shadow-md overflow-hidden">
+            <div className="bg-blue-500 text-white p-4 flex items-center">
+            <img  src='back.png'  alt="React Logo" className="h-6 w-6 mr-2" />
+
+              <h2 className="text-xl font-semibold">backend</h2>
+            </div>
+            <div className="bg-gray-100 p-4">
+            <ul className="text-gray-700">
+                  <li>Django</li>
+                  <li>Python</li>
+                  <li>Restful APIs</li>
+                  <li>MySQL</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+        <div className=" transform transition-transform duration-500 hover:scale-110 hover:rotate-3">
+        <div className=" max-w-sm mx-auto bg-white border border-gray-300 rounded-lg shadow-md overflow-hidden">
+            <div className="bg-blue-500 text-white p-4">
+              <h2 className="text-xl font-semibold">my title</h2>
+            </div>
+            <div className="bg-gray-100 p-4">
+            <ul className="text-gray-700">
+                  <li>Choreo.dev</li>
+                  <li>Python</li>
+                  <li>Restful APIs</li>
+                  <li></li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MySkills;
